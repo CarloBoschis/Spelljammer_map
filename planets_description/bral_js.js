09b1f019d6ebb9f72e8cea06276883ba9f15ba34
deadlybanana <the3incomod@gmail.com>
2022-09-21 14:50:01 +0200

    test mappa finale

--- planets_description/bral_js.js
+++ planets_description/bral_js.js
@@ -4,7 +4,6 @@ d3.html("topside_bral.svg").then(function (newDocument) {
 
     let zoom = d3.zoom()
         .on('zoom', handleZoom)
-        .translateExtent([[-1000, -500], [3000, 1500]])
         .scaleExtent([0.25, 10]);
 
     function handleZoom(e) {
