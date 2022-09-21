7967767c1b0ae1ff744eff2357c57ffdf464e487
deadlybanana <the3incomod@gmail.com>
2022-09-22 01:51:30 +0200

    teeest

--- planets_description/bral_js.js
+++ planets_description/bral_js.js
@@ -19,6 +19,27 @@ d3.html("topside_bral.svg").then(function (newDocument) {
     initZoom()
 })
 
+d3.html("UndersideBral.svg").then(function (newDocument) {
+    const svg = newDocument.querySelector("svg");
+    document.querySelector("#visualization-container2").appendChild(svg);
+
+    let zoom = d3.zoom()
+        .on('zoom', handleZoom)
+        .translateExtent([[0, 0], [20868, 14738]])
+        .scaleExtent([1, 5]);
+
+    function handleZoom(e) {
+        d3.select('svg g')
+            .attr('transform', e.transform);
+    }
+
+    function initZoom() {
+        d3.select('svg')
+            .call(zoom);
+    }
+    initZoom()
+})
+
 function infoTab(id) {
     // Declare all variables
     var i, tabcontent;
