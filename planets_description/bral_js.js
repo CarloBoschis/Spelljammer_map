edcdb3ee6a4da5743f4bd8608f602c98f5bebe6c
deadlybanana <the3incomod@gmail.com>
2022-09-25 14:08:48 +0200

    goddamit

--- planets_description/bral_js.js
+++ planets_description/bral_js.js
@@ -35,6 +35,26 @@ d3.html("topside_bral.svg").then(function (newDocument) {
     initZoom()
 })
 
+d3.html("underside_bral.svg").then(function (newDocument) {
+    const svg2 = newDocument.querySelector("svg");
+    document.querySelector("#undersidemap").appendChild(svg2);
+    let zoom = d3.zoom()
+        .on('zoom', handleZoom)
+        .translateExtent([[0, 0], [20868, 14738]])
+        .scaleExtent([1, 5]);
+
+    function handleZoom(e) {
+        d3.select('svg g')
+            .attr('transform', e.transform);
+    }
+
+    function initZoom() {
+        d3.select('svg2')
+            .call(zoom);
+    }
+    initZoom()
+})
+
 function infoTab(id) {
     // Declare all variables
     var i, tabcontent;
