90137507b3e149d56878c7b206b13144f8f67e60
deadlybanana <the3incomod@gmail.com>
2022-09-25 13:20:50 +0200

    nnesimo test

--- planets_description/bral_js.js
+++ planets_description/bral_js.js
@@ -38,21 +38,6 @@ d3.html("topside_bral.svg").then(function (newDocument) {
 d3.html("underside_bral.svg").then(function (newDocument) {
     const svg = newDocument.querySelector("svg");
     document.querySelector("#undersidemap").appendChild(svg);
-    let zoom = d3.zoom()
-        .on('zoom', handleZoom)
-        .translateExtent([[0, 0], [20868, 14738]])
-        .scaleExtent([1, 5]);
-
-    function handleZoom(e) {
-        d3.select('svg g')
-            .attr('transform', e.transform);
-    }
-
-    function initZoom() {
-        d3.select('svg')
-            .call(zoom);
-    }
-    initZoom()
 })
 
 function infoTab(id) {
